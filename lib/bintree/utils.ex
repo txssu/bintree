881dca96437ea67bf588587f0f924cd9c9efcdbe
Evgeniy Rybin <z.ribin20@gmail.com>
2021-08-11 23:41:31 +0500

    Update remove_duplicates algorithm

--- lib/bintree/utils.ex
+++ lib/bintree/utils.ex
@@ -9,22 +9,23 @@ defmodule Bintree.Utils do
   @doc since: "1.0.0"
   @spec remove_duplicates(Bintree.t()) :: Bintree.t()
   def remove_duplicates(tree) do
-    table = :ets.new(:filtering_duplicates, [])
-    result = do_remove_duplicates(tree, table)
-    :ets.delete(table)
+    {result, _} = do_remove_duplicates(tree, Map.new())
+
     result
   end
 
-  @spec do_remove_duplicates(Bintree.t() | nil, :ets.tid()) :: Bintree.t() | nil
-  defp do_remove_duplicates(nil, _table), do: nil
+  @spec do_remove_duplicates(Bintree.t() | nil, map) :: {Bintree.t() | nil, map}
+  defp do_remove_duplicates(nil, finded), do: {nil, finded}
 
-  defp do_remove_duplicates(%Bintree{value: v, left: l, right: r}, table) do
-    if :ets.insert_new(table, {v}) do
-      left = do_remove_duplicates(l, table)
-      right = do_remove_duplicates(r, table)
-      Bintree.new(v, left, right)
-    else
-      nil
+  defp do_remove_duplicates(tree, finded) do
+    case Map.fetch(finded, tree.value) do
+      {:ok, _} ->
+        {nil, finded}
+      :error ->
+        finded = Map.put_new(finded, tree.value, 0)
+        {left, finded} = do_remove_duplicates(tree.left, finded)
+        {right, finded} = do_remove_duplicates(tree.right, finded)
+        {Bintree.new(tree.value, left, right), finded}
     end
   end
 end
